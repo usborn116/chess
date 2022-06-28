1ce3a0750958dd7ba8f5da176487edda1f538e2f
Usborn Ocampo <usborn116@gmail.com>
2022-06-28 09:30:52 -0700

    knight travails

---
+++ knight_travails.rb
@@ -0,0 +1,61 @@
+class Knight
+  attr_accessor :position, :moves, :children, :parent
+
+  MOVES = [[1,2], [2,1], [2,-1], [1,-2], [-1,-2], [-2,-1], [-2,1], [-1,2]].freeze
+
+  def initialize(position, parent = nil)
+    @position = position
+    @moves = possible_moves(@position)
+    @children = []
+    @parent = parent
+  end
+
+  def possible_moves(position, result = [])
+    next_moves = MOVES.map do |move|
+      move.each_with_index.map { |n, i| n + @position[i] unless (n + @position[i]).negative? || (n + @position[i]) > 8 }
+    end
+    next_moves.delete_if { |move| move.include?(nil) }
+    next_moves
+  end
+end
+
+class Board
+  def knight_moves(start, end_pos)
+    current = make_tree(start, end_pos)
+    history = []
+    make_history(current, history, start)
+    print_knight_moves(history, start, end_pos)
+  end
+ 
+  def make_tree(start, end_pos)
+    queue = [Knight.new(start)]
+    current = queue.shift
+    until current.position == end_pos
+      current.moves.each do |move|
+        current.children << knight = Knight.new(move, current)
+        queue << knight
+      end
+      current = queue.shift
+    end
+    current
+  end
+
+  def make_history(current, history, start)
+    until current.position == start
+      history << current.position
+      current = current.parent
+    end
+    history << current.position
+  end
+
+  def print_knight_moves(history, start, destination)
+    puts "You made it in #{history.size - 1} moves!"
+    puts "To get from #{start} to #{destination} you must traverse the following path:"
+    history.reverse.each { |move| puts move.to_s }
+  end
+end
+
+game = Board.new
+
+game.knight_moves([0, 0], [3, 3])
+puts ''
